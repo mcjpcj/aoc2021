2c218aad4edbe8b26e8c20f49c81a8007a9282db
MP <>
2021-12-07 13:21:02 +0100

    day07 faster solution

--- day07/day07.py
+++ day07/day07.py
@@ -1,15 +1,10 @@
 import numpy as np
 crabs = np.genfromtxt("input.txt", delimiter=",", dtype=int)
-
 crabs_count = {crab: np.count_nonzero(crabs == crab) for crab in set(crabs)}
 # Part one
-costs = []
-for desired_pos in range(0, max(crabs)):
-    costs.append(np.sum([np.abs(crab_pos - desired_pos) * count for crab_pos, count in crabs_count.items()]))
-print(min(costs))
+desired_pos = np.sort(crabs)[crabs.size // 2]
+print(np.sum([np.abs(crab_pos - desired_pos) * count for crab_pos, count in crabs_count.items()]))
 # Part two
-costs = []
-dists = {d: d * (d +1 ) / 2 for d in range(min(crabs),max(crabs)+1)}
-for desired_pos in range(0, max(crabs)):
-    costs.append(np.sum([dists[np.abs(crab_pos - desired_pos)] * count for crab_pos, count in crabs_count.items()]))
-print(min(costs))
+desired_pos = int(np.mean(crabs))
+metric = lambda x: int(x * (x + 1) / 2)
+print(np.sum([metric(np.abs(crab_pos - desired_pos)) * count for crab_pos, count in crabs_count.items()]))
